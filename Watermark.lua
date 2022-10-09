9bfbc7164bea7ab61d442da3b9357c742f30b44d
NexulaDev <112279985+NexulaDev@users.noreply.github.com>
2022-10-09 14:33:53 +0100

    Create Watermark.lua

---
+++ Watermark.lua
@@ -0,0 +1,5 @@
+--Runs the Watermark on Injection.
+game.StarterGui:SetCore("SendNotification", {
+            Title = "Injected";
+            Text = "Thanks for choosing Nexula!;
+            Duration = 3;})
